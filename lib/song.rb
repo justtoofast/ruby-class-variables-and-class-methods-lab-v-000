972ae430b23f1d1432e1c6a8fff9f09aa14b2ce2
Trac Nguyen <nguyendtrac@gmail.com>
2018-03-01 19:29:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,15 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
+  @@genres = []
 
-  def initialize(song)
+  def new(name, artist, genre)
     @song = song
     @@count += 1
+    @@genres << genre
+  end
+
+  def count
+    @@count
   end
 end
